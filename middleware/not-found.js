a3b9dacbe3b110300a418e26d5c0f175b02c474e
mspil88 <matthewspillane88@gmail.com>
2022-03-31 18:31:31 +0100

    mw not found bug fix

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,6 +1,5 @@
-const notFound = (req, res) => {
-    res.status(404).send("route does not exist")
-};
+const notFound = (req, res) => res.status(404).send("route does not exist");
 
 
-modules.export = notFound;
+
+module.exports = notFound;
