8a7e39413d9857e20921cd21685bba3c81bb4100
mspil88 <matthewspillane88@gmail.com>
2022-03-31 18:58:56 +0100

    sleep controller initial

---
+++ controllers/sleep_data.js
@@ -0,0 +1,29 @@
+const getAllSleep = async(req, res) => {
+    res.send("get all sleep");
+}
+
+const getSleep = async(req, res) => {
+    res.send("get sleep");
+}
+
+const createSleep = async(req, res) => {
+    res.send("get all sleep");
+}
+
+const updateSleep = async(req, res) => {
+    res.send("get all sleep");
+}
+
+const deleteSleep = async(req, res) => {
+    res.send("get all sleep");
+}
+
+
+module.exports = {
+    getAllSleep,
+    getSleep,
+    createSleep,
+    updateSleep,
+    deleteSleep
+}
+
