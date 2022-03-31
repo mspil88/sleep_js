464c85cab5117a95caab02b3af255e2a927b61bb
mspil88 <matthewspillane88@gmail.com>
2022-03-31 18:31:16 +0100

    bad request bug fix

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,7 +1,7 @@
 const {StatusCodes} = require("http-status-codes");
 const CustomAPIError = require("./custom-api");
 
-class BadRequestError extends CustomerAPIError {
+class BadRequestError extends CustomAPIError {
     constructor(message) {
         super(message);
         this.StatusCodes = StatusCodes.BAD_REQUEST;
