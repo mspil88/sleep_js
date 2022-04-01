2d322ac8ae9128f7ccb898b69cfb04ce74e29b87
mspil88 <matthewspillane88@gmail.com>
2022-04-01 16:31:32 +0100

    validate password

--- models/User.js
+++ models/User.js
@@ -37,4 +37,9 @@ UserSchema.methods.createJWT = function() {
     return jwt.sign({userId: this._id,name:this.name}, process.env.JWT_SECRET, {expiresIn:process.env.JWT_LIFETIME,})
 }
 
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+    const match = await bcrypt.compare(candidatePassword, this.password);
+    return match;
+}
+
 module.exports = mongoose.model("User", UserSchema);
