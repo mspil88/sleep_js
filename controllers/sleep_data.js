dca49b1f042a7e5a9bf5c768962caea31b98a6eb
mspil88 <matthewspillane88@gmail.com>
2022-04-01 19:15:08 +0100

    set up sleep API

--- controllers/sleep_data.js
+++ controllers/sleep_data.js
@@ -1,22 +1,63 @@
+const Sleep = require("../models/Sleep");
+const {StatusCodes} = require("http-status-codes");
+const {BadRequestError, NotFoundError} = require("../errors");
+
 const getAllSleep = async(req, res) => {
-    res.send("get all sleep");
+    const sleep = await Sleep.find({createdBy: req.user.userId}).sort("createdOn");
+    res.status(StatusCodes.OK).json({sleep, count: sleep.length});
+
 }
 
 const getSleep = async(req, res) => {
-    res.send("get sleep");
+    const {user:{userId}, params:{id:sleepId}} = req;
+    const sleep = await Sleep.findOne({
+        _id: sleepId, createdBy: userId
+    })
+    if(!sleep) {
+        throw new NotFoundError(`Have no found ${sleepId}`);
+    }
+    res.status(StatusCodes.OK).json({sleep});
 }
 
 const createSleep = async(req, res) => {
-    console.log("post sleep")
-    res.json(req.user);
+    req.body.createdBy = req.user.userId;
+    req.body.createdOn = new Date(Date.now());
+    const sleep = await Sleep.create(req.body);
+    res.status(400).json({sleep});
 }
 
 const updateSleep = async(req, res) => {
-    res.send("get all sleep");
-}
+    console.log("update sleep");
+    const {
+        body: {bedTime, timeToFallAsleep, numberTimesAwake, amountTimeAwake, timeGotOutBed, timeToGetOutBed, qualityOfSleep, nextDayFeeling, hoursSpentInBed, hoursSpentAsleep, sleepEfficiencyScore},
+        user:{userId}, 
+        params:{id:sleepId}} = req;
+
+    if(bedTime === "" || timeToFallAsleep === ""|| numberTimesAwake === ""|| amountTimeAwake === ""|| timeGotOutBed === ""|| timeToGetOutBed === ""|| qualityOfSleep === ""||
+        nextDayFeeling === ""|| hoursSpentInBed === ""||hoursSpentAsleep === ""||sleepEfficiencyScore === "") {
+            throw new BadRequestError("Insufficient parameters passed")
+        }
+    const sleep = await Sleep.findByIdAndUpdate({_id: sleepId, createdBy: userId}, req.body, {new:true, runValidators: true})
+    console.log(sleep);
+    if(!sleep) {
+        throw new NotFoundError(`Have no found ${sleepId}`);
+    }
+
+    res.status(StatusCodes.OK).json({sleep});
+    }
 
 const deleteSleep = async(req, res) => {
-    res.send("get all sleep");
+    console.log("delete sleep");
+    const {user:{userId}, params:{id:sleepId}} = req;
+
+    const sleep = await Sleep.findOneAndRemove({
+        _id: sleepId,
+        createdBy: userId
+    })
+    if(!sleep) {
+        throw new NotFoundError(`Have no found ${sleepId}`);
+    }
+    res.status(StatusCodes.OK).send();
 }
 
 
---
+++ models/Sleep.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const SleepSchema = new mongoose.Schema({
+    bedTime: {
+        type:String,
+        required:[true, 'Please provide bed time'],
+        maxLength: 7
+    },
+    timeToFallAsleep: {
+        type:String,
+        required:[true],
+        maxLength: 30
+    },
+    numberTimesAwake: {
+        type:String,
+        required:[true],
+        maxLength: 30
+    },
+    amountTimeAwake: {
+        type:String,
+        required:[true],
+        maxLength: 30
+    },
+    timeGotOutBed: {
+        type:String,
+        required:[true],
+        maxLength: 7
+    },
+    timeToGetOutBed: {
+        type:String,
+        required:[true],
+        maxLength: 30
+    },
+    qualityOfSleep: {
+        type:String,
+        required:[true],
+        maxLength: 8
+    },
+    nextDayFeeling: {
+        type:String,
+        required:[true],
+        maxLength: 30
+    },
+    hoursSpentInBed: {
+        type:mongoose.Types.Decimal128,
+        required:[true],
+    },
+    hoursSpentAsleep: {
+        type:mongoose.Types.Decimal128,
+        required:[true],
+    },
+    sleepEfficiencyScore: {
+        type:mongoose.Types.Decimal128,
+        required:[true],
+    },
+    createdBy: {
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:[true]
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model("Sleep", SleepSchema);
