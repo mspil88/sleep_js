3489f6c4dc3d2c805d540badb6d230f6245453fe
mspil88 <matthewspillane88@gmail.com>
2022-04-01 16:15:16 +0100

    init login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,7 @@
 const User = require("../models/User");
 const {StatusCodes} = require("http-status-codes");
 const bcrypt = require("bcryptjs");
-
-
+const {BadRequestError, UnauthenticatedError} = require("../errors");
 
 const register = async(req, res) => {
     const user = await User.create({...req.body});
@@ -11,7 +10,19 @@ const register = async(req, res) => {
 }
 
 const login = async(req, res) => {
-    res.send("login user");
+    const {email, password} = req.body;
+
+    if((!email) || (!password)) {
+        throw new BadRequestError("Please provide email and password");
+    }
+
+    const user = await User.findOne({email});
+    if(!user) {
+        throw new UnauthenticatedError("User does not exist")
+    }
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({user: {name: user.name}, token});
 }
 
 module.exports = {
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const {register, login} = require("../controllers/auth");
 
 router.post("/register", register);
-router.post("/register", login);
+router.post("/login", login);
 
 module.exports = router;
