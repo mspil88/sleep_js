dd526bed7c0e9682d054580b541dcbf5a11e4958
mspil88 <matthewspillane88@gmail.com>
2022-04-02 11:23:42 +0100

    implemented api for mood route

--- routes/mood.js
+++ routes/mood.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 
 const {getAllMoods, getMood, postMood, updateMood, deleteMood} = require("../controllers/mood_data");
 
