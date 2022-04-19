7f1ce7886bcabefebf15236fee44388caa2cfeaf
mspil88 <matthewspillane88@gmail.com>
2022-04-19 10:43:10 +0100

    implemented dynamic addition of tasks with assignment of draggable functionality

--- public/relax.js
+++ public/relax.js
@@ -1,22 +1,37 @@
-const draggables = document.querySelectorAll('.draggable')
-const containers = document.querySelectorAll('.wind-down-draggable')
-const deleteBtns = document.querySelectorAll('.fa-trash-alt')
+const draggables = document.querySelectorAll(".draggable");
+const containers = document.querySelectorAll(".wind-down-draggable");
+const deleteBtns = document.querySelectorAll(".fa-trash-alt");
+const wdTasks = document.querySelector(".wd-task");
+const addTaskBtn = document.querySelector(".add-task-btn");
+const taskInput = document.querySelector(".task-input");
 
-draggables.forEach(draggable => {
-  draggable.addEventListener('dragstart', () => {
-    draggable.classList.add('dragging')
-  })
+let draggablesArray = Array.from(draggables)
 
-  draggable.addEventListener('dragend', () => {
-    draggable.classList.remove('dragging')
-  })
-})
+const assignDraggableEvents = (draggablesArray) => {
+    draggablesArray.forEach(draggable => {
+    console.log("DRAGGABLE LIST")
+    console.log(draggablesArray);
+    draggable.addEventListener("dragstart", () => {
+        console.log("dragstart")    
+        draggable.classList.add("dragging")
+    })
+
+    draggable.addEventListener("dragend", () => {
+        console.log("dragstart")
+        draggable.classList.remove("dragging")
+    })
+    })
+}
+
+assignDraggableEvents(draggablesArray);
 
 containers.forEach(container => {
-  container.addEventListener('dragover', e => {
+  container.addEventListener("dragover", e => {
     e.preventDefault()
     const afterElement = getDragAfterElement(container, e.clientY)
-    const draggable = document.querySelector('.dragging')
+    console.log(afterElement);
+    const draggable = document.querySelector(".dragging")
+    console.log(draggable);
     if (afterElement == null) {
       container.appendChild(draggable)
     } else {
@@ -26,7 +41,7 @@ containers.forEach(container => {
 })
 
 function getDragAfterElement(container, y) {
-  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+  const draggableElements = [...container.querySelectorAll(".draggable:not(.dragging)")]
 
   return draggableElements.reduce((closest, child) => {
     const box = child.getBoundingClientRect()
@@ -44,3 +59,33 @@ deleteBtns.forEach((btn)=> {
         console.log("delete me")
     })
 })
+
+// add task
+    //create task
+
+const createTask = (taskName) => {
+    taskElem = document.createElement("p");
+    taskElem.className = "draggable";
+    taskElem.draggable = true;
+    
+    taskElem.innerHTML = `${taskName} <i class="fas fa-trash-alt">`;
+    console.log(taskElem);
+    wdTasks.appendChild(taskElem);
+    draggablesArray.push(taskElem);
+    console.log(draggablesArray);
+    assignDraggableEvents(draggablesArray);
+
+}
+
+const treatInput = (text) => {
+    return text.trim();
+} 
+
+addTaskBtn.addEventListener("click", ()=> {
+    console.log("add btn");
+    console.log(taskInput.value);
+    createTask(treatInput(taskInput.value));
+})
+
+// save schedule
+
