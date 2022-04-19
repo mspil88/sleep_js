b1b5276d7dd02127e9ca0dc2cd099c32fba303c5
mspil88 <matthewspillane88@gmail.com>
2022-04-19 20:44:40 +0100

    tasks schema

---
+++ models/Tasks.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+    tasks: {
+        type:String,  
+    },
+    scheduledTasks: {
+        type:String
+    },
+    createdBy: {
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:[true]
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model("tasks", TaskSchema);
